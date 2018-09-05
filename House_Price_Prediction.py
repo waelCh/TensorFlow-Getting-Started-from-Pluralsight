d17a25d89a6c1a2fb22ababfae4cdc0c94e055cb
unknown <wael.imaginecup@gmail.com>
2018-09-05 21:52:16 +0100

    Splitted data between Training data and testing data

--- House_Price_Prediction.py
+++ House_Price_Prediction.py
@@ -25,3 +25,42 @@ plt.plot(house_size, house_price, "bx") #   bx = blue x
 plt.ylabel("Price")
 plt.xlabel("size")
 plt.show()
+
+#   Normalizer function to prevent under/overflows
+def normalize(array):
+    return (array - array.mean()) / array.std()
+
+#   Define number of training sample, 0.7 = 70%; we can take the first 70% since we are randomized
+num_train_samples = math.floor(num_house * 0.7)
+
+#   Define training data
+train_house_size = np.asarray(house_size[:num_train_samples])
+train_price = np.asanyarray(house_price[:num_train_samples:])
+
+train_house_size_norm = normalize(train_house_size)
+train_price_norm = normalize(train_house_size)
+
+#   Define test data
+test_house_size = np.asarray(house_size[num_train_samples:])
+test_house_price = np.array(house_price[num_train_samples:])
+
+test_house_price_norm = normalize(test_house_price)
+test_house_size_norm = normalize(test_house_size)
+
+#   Set up the placeholders that get updated as we desceed down the greadient
+tf_house_size = tf.placeholder("float", name="house_size")
+tf_price = tf.placeholder("float", name="price")
+
+#   Define the variables holding the size_factor and price we set during training
+#   We initialize them to some random values based on the normal distribuion 
+tf_size_factor = tf.Variable(np.random.randn(), name="size_factor")
+tf_price_offset = tf.Variable(np.random.randn(), name="price_offset")
+
+#   Define the operation for the predicting values - predicted price = (size_factor * house_size) + price_offset
+#   Notice the use of the Tensorflow add and multiply functon.
+#   These add the operation to the tensorflow computation graph
+tf_price_pred = tf.add(tf.multiply(tf_size_factor, house_size), tf_price_offset)
+
+#   Define the loss function (how much error) - Mean squared error
+tf_cost = tf.reduce_sum(tf.pow(tf_price_pred - tf_price, 2))/(2*num_train_samples)
+
