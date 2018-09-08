12e1ce6496ec3326b30592b88cbd515692729aca
unknown <wael.imaginecup@gmail.com>
2018-09-08 17:39:27 +0100

    final version with animation

--- House_Price_Prediction.py
+++ House_Price_Prediction.py
@@ -18,7 +18,7 @@ house_size = np.random.randint(low=1000, high=3500, size=num_house )
 
 # Generate house prices from house size with a random noise added.
 np.random.seed(42)
-house_price = house_size * 100.0 + np.random.randint(low=20000, high=70000, size=num_house)  
+house_price = house_size * 100.0 + np.random.randint(low=20000, high=70000, size=num_house) 
 
 # Plot generated house and size 
 plt.plot(house_size, house_price, "bx")  # bx = blue x
