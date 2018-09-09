20d8c653b3eaee96804d3c75aa70df8ef1478e16
unknown <wael.imaginecup@gmail.com>
2018-09-09 22:23:01 +0100

    Implement Deep MNIST version

--- SimpleMNIST.py
+++ SimpleMNIST.py
@@ -24,7 +24,7 @@ b = tf.Variable(tf.zeros([10]))
 y = tf.nn.softmax(tf.matmul(x, W) + b)
 #   loss is cross entropy
 cross_entropy = tf.reduce_mean(
-                tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
+                tf.nn.softmax_cross_entropy_with_logits_v2(labels=y_, logits=y))
 
 #   Each training step in gradient discent we want to minimize cross entropy
 train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
@@ -48,7 +48,8 @@ for i in range(1000):
 
 #   Evaluate how well the model did. Do this by comparing the digit with the highest probability 
 #   in actual (y) and predicted (y_).
-correct_prediction = tf.equal(tf.argmax(y,1), tf.arg_max(y_, 1))
+correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_, 1))
+print(correct_prediction)
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 test_accuracy = sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels})
 print("Test Accuracy: {0}%".format(test_accuracy * 100.0))
