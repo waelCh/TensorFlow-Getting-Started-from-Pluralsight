2cc6bdc074064704aea70e63fa0e18d4916562b3
unknown <wael.imaginecup@gmail.com>
2018-09-15 06:46:34 +0100

    Added name and name_scope to the code to utilize the TensorBoard

--- DeepMNIST.py
+++ DeepMNIST.py
@@ -1,6 +1,9 @@
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 
+# Define path to TensorBoard log files
+logPath = "./tb_logs/"
+
 # Create input object which reads data from MNIST datasets.  Perform one-hot encoding to define the digit
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 
@@ -8,8 +11,9 @@ mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 sess = tf.InteractiveSession()
 
 # Define placeholders for MNIST input data
-x = tf.placeholder(tf.float32, shape=[None, 784]) 
-y_ = tf.placeholder(tf.float32, [None, 10])
+with tf.name_scope("MNIST_input"):
+    x = tf.placeholder(tf.float32, shape=[None, 784], name="x") 
+    y_ = tf.placeholder(tf.float32, [None, 10], name="y_")
 
 # change the MNIST input data from a list of values to a  28 pixel X 28 pixel X 1 grayscale value cube
 #    which the Convolution NN can use.
@@ -19,44 +23,46 @@ x_image = tf.reshape(x, [-1,28,28,1], name="x_image")
 # Define helper functions to created weights and baises variables, and convolution, and pooling layers
 #   We are using RELU as our activation function.  These must be initialized to a small positive number 
 #   and with some noise so you don't end up going to zero when comparing diffs
-def weight_variable(shape):
+def weight_variable(shape, name=None):
     initial = tf.truncated_normal(shape, stddev=0.1)
-    return tf.Variable(initial)
+    return tf.Variable(initial, name=name)
 
-def bias_variable(shape):
+def bias_variable(shape, name=None):
     initial = tf.constant(0.1, shape=shape)
-    return tf.Variable(initial)
+    return tf.Variable(initial, name=name)
 
 #   Convolution and Pooling - we do Convolution, and then pooling to control overfitting
-def conv2d(x, W):
-    return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+def conv2d(x, W, name=None):
+    return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME', name=name)
 
-def max_pool_2x2(x):
+def max_pool_2x2(x, name=None):
     return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
-                        strides=[1, 2, 2, 1], padding='SAME')
+                        strides=[1, 2, 2, 1], padding='SAME', name=name)
 
 # Define layers in the NN
 
 # 1st Convolution layer
-# 32 features for each 5X5 patch of the image
-W_conv1 = weight_variable([5, 5, 1, 32])
-b_conv1 = bias_variable([32])
-# Do convolution on images, add bias and push through RELU activation
-h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
-print("h_conv1: ", h_conv1)
-# take results and run through max_pool
-h_pool1 = max_pool_2x2(h_conv1)
-print("h_pool1: ", h_pool1)
+with tf.name_scope("conv1"):
+    # 32 features for each 5X5 patch of the image
+    W_conv1 = weight_variable([5, 5, 1, 32], name="weight")
+    b_conv1 = bias_variable([32], name="bias")
+    # Do convolution on images, add bias and push through RELU activation
+    h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1, name="conv2d") + b_conv1, name="relu")
+    print("h_conv1: ", h_conv1)
+    # take results and run through max_pool
+    h_pool1 = max_pool_2x2(h_conv1, name="pool")
+    print("h_pool1: ", h_pool1)
 
 # 2nd Convolution layer
-# Process the 32 features from Convolution layer 1, in 5 X 5 patch.  Return 64 features weights and biases
-W_conv2 = weight_variable([5, 5, 32, 64])
-b_conv2 = bias_variable([64])
-# Do convolution of the output of the 1st convolution layer.  Pool results 
-h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
-print("h_conv2: ", h_conv2)
-h_pool2 = max_pool_2x2(h_conv2)
-print("h_pool2: ", h_pool2)
+with tf.name_scope("conv2"):
+    # Process the 32 features from Convolution layer 1, in 5 X 5 patch.  Return 64 features weights and biases
+    W_conv2 = weight_variable([5, 5, 32, 64], name="weight")
+    b_conv2 = bias_variable([64], name="bias")
+    # Do convolution of the output of the 1st convolution layer.  Pool results 
+    h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2, name="conv2d") + b_conv2, name="relu")
+    print("h_conv2: ", h_conv2)
+    h_pool2 = max_pool_2x2(h_conv2, name="pool")
+    print("h_pool2: ", h_pool2)
 
 # Fully Connected Layer
 W_fc1 = weight_variable([7 * 7 * 64, 1024])
@@ -72,31 +78,38 @@ keep_prob = tf.placeholder(tf.float32)  # get dropout probability as a training 
 h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
 
 # Readout layer
-W_fc2 = weight_variable(h_pool2_flat024, 10])
-b_fc2 = bias_variable([10])
+with tf.name_scope("Readout"):
+    W_fc2 = weight_variable([1024, 10], name="weight")
+    b_fc2 = bias_variable([10], name="bias")
 
 # Define model
 y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
 
 # Loss measurement
-cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=y_conv, labels=y_))
+with tf.name_scope("cross_entropy"):
+    cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=y_conv, labels=y_))
 
 # loss optimization
-train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+with tf.name_scope("loss_optimizer"):
+    train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
 
-# What is correct
-correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
-# How accurate is it?
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+with tf.name_scope("accuracy"):
+    # What is correct
+    correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
+    # How accurate is it?
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
 # Initialize all of the variables
 sess.run(tf.global_variables_initializer())
+
+#   TB - Write the default graph out so we can view it's structure
+tbWriter = tf.summary.FileWriter(logPath, sess.graph)
 
 # Train the model
 import time
 
 #  define number of steps and how often we display progress
-num_steps = 3000
+num_steps = 2000
 display_every = 100
 
 # Start timer
